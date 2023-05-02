ea8211736365455a9206addea6fb564bfc8084d3
enrique <enrique.martinez-acevedo@ucdenver.edu>
2023-05-02 11:46:30 -0600

    added new files

---
+++ Q2.py
@@ -0,0 +1,13 @@
+from heapq import heappush, heappop, heapify
+from collections import defaultdict
+from bitarray import bitarray, decodetree
+import math
+import os
+
+
+
+def user_input():
+    print("Welcome to Algorithms HW6 question #3")
+    user_selection = input("Please input text or input file: ")
+
+user_input()
